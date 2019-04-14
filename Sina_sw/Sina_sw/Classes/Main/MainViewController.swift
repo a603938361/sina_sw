df1fb8bf6855ffdad95a8327299782f1bbd5a87b
shunli.huang <shunli.huang@connext.com.cn>
2019-04-14 22:44:36 +0800

    通过反射机制创建class

--- Sina_sw/Sina_sw/Classes/Main/MainViewController.swift
+++ Sina_sw/Sina_sw/Classes/Main/MainViewController.swift
@@ -13,18 +13,34 @@ class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
                 
-        addChild(childVC: HomeViewController(), title: "首页", image: "tabbar_home")
-        addChild(childVC: MessageViewController(), title: "消息", image: "tabbar_message_center")
-        addChild(childVC: DiscoverViewController(), title: "发现", image: "tabbar_discover")
-        addChild(childVC: ProfileViewController(), title: "我的", image: "tabbar_profile")
+        addChild(childVCName: "HomeViewController", title: "首页", image: "tabbar_home")
+        addChild(childVCName: "MessageViewController", title: "消息", image: "tabbar_message_center")
+        addChild(childVCName: "DiscoverViewController", title: "发现", image: "tabbar_discover")
+        addChild(childVCName: "ProfileViewController", title: "我的", image: "tabbar_profile")
 
     }
     
-    private func addChild(childVC: UIViewController, title:String, image:String) {
+//    根据字符串创建VC
+    private func addChild(childVCName: String, title:String, image:String) {
         
-        guard   else {
-            <#statements#>
+//        在info.plist 找到项目名称（命名空间）
+        guard let nameSpace = Bundle.main.infoDictionary!["CFBundleExecutable"] as? String else {
+            return
         }
+        
+//        通过反射机制创建Anyclass （命名空间+.+className）
+        guard let vcClass = NSClassFromString(nameSpace + "." + childVCName) else {
+            print("没有找到class")
+            return
+        }
+        
+//      AnyClass 转换成 UIViewController
+        guard let childVcType = vcClass as? UIViewController.Type else{
+            return
+        }
+        
+//        初始化UIViewController
+        let childVC = childVcType.init()
         
         childVC.title = title
         childVC.tabBarItem.image = UIImage(named: image)
